c1c11cdb99c3018df05a1a2ecc5cc08b56d89631
seeflood <349895584@qq.com>
2022-01-11 14:14:26 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# python-sdk
+python sdk for layotto
+
---
+++ src/client/grpc/client.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+import grpc  # type: ignore
+from grpc import (  # type: ignore
+    UnaryUnaryClientInterceptor,
+    UnaryStreamClientInterceptor,
+    StreamUnaryClientInterceptor,
+    StreamStreamClientInterceptor
+)
+
+from typing import Dict, Optional, Union, Sequence, List
+
+class RuntimeGrpcClient:
+    """The convenient layer implementation of Layotto gRPC APIs.
+    This provides the wrappers and helpers to allows developers to use runtime gRPC API
+    easily and consistently.
+    Examples:
+        >>> from src.client import RuntimeGrpcClient
+        >>> cli = RuntimeGrpcClient("127.0.0.1", 34904)
+        >>> resp = cli.invoke_method('callee', 'method', b'data')
+    With context manager:
+        >>> from src.client import RuntimeGrpcClient
+        >>> with RuntimeGrpcClient("127.0.0.1",34904) as cli:
+        ...     resp = cli.invoke_method('callee', 'method', b'data')
+    """
+
+    def __init__(
+        self,
+        ip: str,
+        port: int,
+        interceptors: Optional[List[Union[
+            UnaryUnaryClientInterceptor,
+            UnaryStreamClientInterceptor,
+            StreamUnaryClientInterceptor,
+            StreamStreamClientInterceptor]]] = None):
+        """Connects to Layotto Runtime and initialize gRPC client stub.
+        Args:
+            ip: The IP address of Layotto Runtime.
+            port: The port of Layotto Runtime.
+            interceptors (list of UnaryUnaryClientInterceptor or
+                UnaryStreamClientInterceptor or
+                StreamUnaryClientInterceptor or
+                StreamStreamClientInterceptor, optional): gRPC interceptors.
+        """
+        self._address = ip+":"+str(port)
+        self._channel = grpc.insecure_channel(self._address)   # type: ignore
+
+        if interceptors:
+            self._channel = grpc.intercept_channel(   # type: ignore
+                self._channel, *interceptors)
+        # TODO set stub
+
+    def close(self):
+        """Closes runtime gRPC channel."""
+        if self._channel:
+            self._channel.close()
+
+    def __del__(self):
+        self.close()
+
+    def __enter__(self) -> 'RuntimeGrpcClient':
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        self.close()
+
+    def say_hello():
+        """Sends a Hello request to Layotto Runtime.
+        Returns:
+            HelloResponse: The response from Layotto Runtime.
+        """
+        # TODO send request to layotto runtime
+        return "hello,world"
+
